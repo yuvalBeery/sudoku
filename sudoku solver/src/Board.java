63e3e764f5c0913c98f618e5d565eed7db53ce3f
u9198081 <u9198081@bsmch.net>
2023-12-23 11:24:21 +0200

    ref(Board.java): use imbeded functions

--- sudoku solver/src/Board.java
+++ sudoku solver/src/Board.java
@@ -1,19 +1,18 @@
+import java.util.Arrays;
+
 public class Board {
     final static int BOARD_SIZE = 9;
     int[][] board = new int[BOARD_SIZE][BOARD_SIZE];
 
     public void compareTo(Board oldBoard) {
-        for (int r = 0; r < BOARD_SIZE; r++) {
-            for (int c = 0; c < BOARD_SIZE; c++)
-            this.board[r][c] = oldBoard.board[r][c];
+        for (int rowIndex = 0; rowIndex < board.length; rowIndex++) {
+            board[rowIndex] = Arrays.copyOf(oldBoard.board[rowIndex], board[rowIndex].length);
         }
     }
 
     public void printBoard() {
-        for (int r = 0; r < BOARD_SIZE; r++) {
-            for (int c = 0; c < BOARD_SIZE; c++)
-                System.out.print(this.board[r][c] + "  ");
-            System.out.println("");
+        for (int[] row : board) {
+            Arrays.toString(row);
         }
     }
 }
